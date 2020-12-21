4f51676fb07e7cae2c35c26874c6f649c6224f5b
acheype <adrien.cheype@gmail.com>
2020-12-21 18:46:06 +0100

    fix(install): remove update action
    because it will in the core in the future

--- handlers/update__.php
+++ handlers/update__.php
@@ -9,7 +9,7 @@
  * @license  https://www.gnu.org/licenses/agpl-3.0.en.html AGPL 3.0
  * @link     https://yeswiki.net
  */
- 
+
 namespace YesWiki;
 
 use YesWiki\Core\Service\Performer;
@@ -23,13 +23,15 @@ if (!defined("WIKINI_VERSION")) {
 /**
  * Constants to define the OuiNon Lms list
  */
-!defined('OUINON_LIST_JSON') && define('OUINON_LIST_JSON', '{"label":{"oui":"Oui","non":"Non"},"titre_liste":"OuiNon Lms"}');
+!defined('OUINON_LIST_JSON') && define('OUINON_LIST_JSON',
+    '{"label":{"oui":"Oui","non":"Non"},"titre_liste":"OuiNon Lms"}');
 
 /**
  * Constants to define the contents of the LMS forms
  */
 !defined('ACTIVITE_FORM_NOM') && define('ACTIVITE_FORM_NOM', 'Activité LMS');
-!defined('ACTIVITE_FORM_DESCRIPTION') && define('ACTIVITE_FORM_DESCRIPTION', 'Activité (fiche de cours, exercice, vidéo, fiche pdf...) utilisée pour le module d\'apprentissage en ligne');
+!defined('ACTIVITE_FORM_DESCRIPTION') && define('ACTIVITE_FORM_DESCRIPTION',
+    'Activité (fiche de cours, exercice, vidéo, fiche pdf...) utilisée pour le module d\'apprentissage en ligne');
 !defined('ACTIVITE_FORM_TEMPLATE') && define('ACTIVITE_FORM_TEMPLATE', 'texte***bf_titre***Titre de l\'activité***255***255*** *** ***text***1*** *** *** *** *** *** ***
 tags***bf_autrices***Auteur·ice·s*** *** *** *** *** ***0*** ***Appuyer sur la touche « Entrée » pour séparer les auteur·ice·s
 texte***bf_duree***Durée estimée de l\'activité en minutes*** *** *** *** ***number***0*** *** *** *** *** *** ***
@@ -41,7 +43,8 @@ reactions***reactions*** *** *** *** *** *** *** *** ***
 acls*** + ***@admins***@admins*** *** *** *** *** *** ***');
 
 !defined('MODULE_FORM_NOM') && define('MODULE_FORM_NOM', 'Module LMS');
-!defined('MODULE_FORM_DESCRIPTION') && define('MODULE_FORM_DESCRIPTION', 'Module (enchaînement d\'activités) utilisé pour le module d\'apprentissage en ligne');
+!defined('MODULE_FORM_DESCRIPTION') && define('MODULE_FORM_DESCRIPTION',
+    'Module (enchaînement d\'activités) utilisé pour le module d\'apprentissage en ligne');
 !defined('MODULE_FORM_TEMPLATE') && define('MODULE_FORM_TEMPLATE', 'texte***bf_titre***Titre du module***255***255*** *** ***text***1*** *** *** *** *** *** ***
 textelong***bf_description***Description***80***4*** *** ***wiki***0*** *** *** *** *** *** ***
 image***bf_image***Image***300***300***600***600***left***0*** ***
@@ -52,7 +55,8 @@ navigationmodule***bf_navigation*** *** *** *** *** *** *** *** ***
 acls*** + ***@admins***@admins*** *** *** *** *** *** ***');
 
 !defined('PARCOURS_FORM_NOM') && define('PARCOURS_FORM_NOM', 'Parcours LMS');
-!defined('PARCOURS_FORM_DESCRIPTION') && define('PARCOURS_FORM_DESCRIPTION', 'Parcours (enchaînement de modules) utilisé pour le module d\'apprentissage en ligne');
+!defined('PARCOURS_FORM_DESCRIPTION') && define('PARCOURS_FORM_DESCRIPTION',
+    'Parcours (enchaînement de modules) utilisé pour le module d\'apprentissage en ligne');
 !defined('PARCOURS_FORM_TEMPLATE') && define('PARCOURS_FORM_TEMPLATE', 'texte***bf_titre***Titre du parcours***255***255*** *** ***text***1*** *** *** *** *** *** ***
 checkboxfiche***' . $GLOBALS['wiki']->config['lms_config']['module_form_id'] . '***Modules*** *** *** ***bf_modules***dragndrop***0*** ***L\'ordre des modules définit le parcours de l\'apprenant*** *** *** *** ***
 liste***ListeOuinonLms***Scénarisation des activités*** *** ***oui***bf_scenarisation_activites*** ***1*** ***Pour valider un module, un apprenant doit avoir valider toutes les activités du module*** *** *** *** ***
@@ -79,7 +83,8 @@ function checkAndAddForm(&$plugin_output_new, $formId, $formName, $formeDescript
         $GLOBALS['wiki']->Query('INSERT INTO ' . $GLOBALS['wiki']->config['table_prefix'] . 'nature (`bn_id_nature` ,`bn_ce_i18n` ,'
             . '`bn_label_nature` ,`bn_template` ,`bn_description` ,`bn_sem_context` ,`bn_sem_type` ,`bn_sem_use_template` ,'
             . '`bn_condition`)'
-            . ' VALUES (' . $formId . ', \'fr-FR\', \'' . mysqli_real_escape_string($GLOBALS['wiki']->dblink, $formName) . '\', \''
+            . ' VALUES (' . $formId . ', \'fr-FR\', \'' . mysqli_real_escape_string($GLOBALS['wiki']->dblink,
+                $formName) . '\', \''
             . mysqli_real_escape_string($GLOBALS['wiki']->dblink, $formTemplate) . '\', \''
             . mysqli_real_escape_string($GLOBALS['wiki']->dblink, $formeDescription) . '\', \'\', \'\', 1, \'\')');
 
@@ -95,7 +100,7 @@ if ($this->UserIsAdmin()) {
     $output .= '<strong>Extension LMS</strong><br/>';
 
     // if the OuiNon Lms list doesn't exist, create it
-    if (!$this->LoadPage('ListeOuinonLms')){
+    if (!$this->LoadPage('ListeOuinonLms')) {
         $output .= 'ℹ️ Adding the <em>OuiNon Lms</em> list<br />';
         // save the page with the list value
         $this->SavePage('ListeOuinonLms', OUINON_LIST_JSON);
@@ -109,36 +114,41 @@ if ($this->UserIsAdmin()) {
     }
 
     // test if the activite form exists, if not, install it
-    checkAndAddForm($output, $GLOBALS['wiki']->config['lms_config']['activite_form_id'], ACTIVITE_FORM_NOM, ACTIVITE_FORM_DESCRIPTION, ACTIVITE_FORM_TEMPLATE);
+    checkAndAddForm($output, $GLOBALS['wiki']->config['lms_config']['activite_form_id'], ACTIVITE_FORM_NOM,
+        ACTIVITE_FORM_DESCRIPTION, ACTIVITE_FORM_TEMPLATE);
     // test if the module form exists, if not, install it
-    checkAndAddForm($output, $GLOBALS['wiki']->config['lms_config']['module_form_id'], MODULE_FORM_NOM, MODULE_FORM_DESCRIPTION, MODULE_FORM_TEMPLATE);
+    checkAndAddForm($output, $GLOBALS['wiki']->config['lms_config']['module_form_id'], MODULE_FORM_NOM,
+        MODULE_FORM_DESCRIPTION, MODULE_FORM_TEMPLATE);
     // test if the parcours form exists, if not, install it
-    checkAndAddForm($output, $GLOBALS['wiki']->config['lms_config']['parcours_form_id'], PARCOURS_FORM_NOM, PARCOURS_FORM_DESCRIPTION, PARCOURS_FORM_TEMPLATE);
+    checkAndAddForm($output, $GLOBALS['wiki']->config['lms_config']['parcours_form_id'], PARCOURS_FORM_NOM,
+        PARCOURS_FORM_DESCRIPTION, PARCOURS_FORM_TEMPLATE);
 
     // if the PageMenuLms page doesn't exist, create it with a default version
     if (!$this->LoadPage('PageMenuLms')) {
         $output .= 'ℹ️ Adding the <em>PageMenuLms</em> page<br />';
-        $this->SavePage('PageMenuLms', '""<div><span>""{{button link="config/root_page" nobtn="1" icon="fas fa-home"}}""</span><span style="float: right;">""{{button link="UserEntries" nobtn="1" text="Accès à mes fiches" icon="far fa-clone"></i>"}}""</span></div>""'
+        $this->SavePage('PageMenuLms',
+            '""<div><span>""{{button link="config/root_page" nobtn="1" icon="fas fa-home"}}""'
+            . '</span><span style="float: right;">""'
+            . '{{button link="UserEntries" nobtn="1" text="Accès à mes fiches" icon="far fa-clone"></i>"}}""</span></div>""'
             . "\n\n" . '{{menuparcours}}');
         $output .= '✅ Done !<br />';
     } else {
         $output .= '✅ The <em>PageMenuLms</em> page already exists.<br />';
     }
-
     $output .= '<hr />';
 }
 
 // Add button to return to previous page
-$tag = $GLOBALS['wiki']->getPageTag() ;
-$vars['link'] = $tag ;
-$vars['text'] = 'Retourner à la page '.$tag ;
-$vars['title'] = 'Retourner à la page '.$tag ;
-$vars['class'] = 'btn-success' ;
-$output .= $GLOBALS['wiki']->services->get(Performer::class)->run('button', 'action', $vars) ;
+$tag = $GLOBALS['wiki']->getPageTag();
+$vars['link'] = $tag;
+$vars['text'] = 'Retourner à la page ' . $tag;
+$vars['title'] = 'Retourner à la page ' . $tag;
+$vars['class'] = 'btn-success';
+$output .= $GLOBALS['wiki']->services->get(Performer::class)->run('button', 'action', $vars);
 
 // add the content before footer
 $plugin_output_new = str_replace(
     '<!-- end handler /update -->',
-    $output.'<!-- end handler /update -->',
+    $output . '<!-- end handler /update -->',
     $plugin_output_new
 );
