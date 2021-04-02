95382c1d316629ee9c22d847846c0cc2277a4f8c
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-04-02 14:17:26 +0200

    feat(activity-condition): gives api location to js

--- presentation/javascript/activity-navigation-view.js
+++ presentation/javascript/activity-navigation-view.js
@@ -1,22 +1,30 @@
 function checkActivityConditions(elem,courseTag, moduleTag, activityTag = ''){
     // add wait cursor
     elem.classList.add("wait-cursor");
-
+   
+    if (!checkActivityConditionsURL){
+        checkActivityConditionsWrong(elem,"<div>Erreur en vérifiant les conditions de passage.</div>");
+    }
     // test wait 2 seconds
     setTimeout(function (){
-        // false case
-        // remove wait cursor
-        // elem.classList.remove("wait-cursor");
-        //right case
-        // set url
-        elem.setAttribute("href","http://localhost/test_LMS/?BienDemarrerSaTransitionVersLesLogiciels");
-        // remove class
-        elem.classList.remove("wait-cursor","disabled");
-        // remove events
-        elem.removeAttribute("onmouseover");
-        elem.addEventListener("click",function (event){
-            event.stopPropagation();
-            window.location.href = this.getAttribute("href");
-        });
+        checkActivityConditionsRight(elem,"http://localhost/test_LMS/?BienDemarrerSaTransitionVersLesLogiciels");
     },2000);
+}
+
+function checkActivityConditionsWrong(elem,message){
+    //remove wait cursor
+    elem.classList.remove("wait-cursor");
+}
+
+function checkActivityConditionsRight(elem,url){
+    // set url
+    elem.setAttribute("href",url);
+    // remove class
+    elem.classList.remove("wait-cursor","disabled");
+    // remove events
+    elem.removeAttribute("onmouseover");
+    elem.addEventListener("click",function (event){
+        event.stopPropagation();
+        window.location.href = this.getAttribute("href");
+    });
 }
